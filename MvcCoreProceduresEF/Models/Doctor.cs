d8e951b30dd3de3c8baf129f2f7b980404d18c69
AlvaroCascoV <alvarocascovalero@gmail.com>
2026-02-11 14:18:52 +0100

    buenas practicas

--- MvcCoreProceduresEF/Models/Doctor.cs
+++ MvcCoreProceduresEF/Models/Doctor.cs
@@ -6,11 +6,12 @@ namespace MvcCoreProceduresEF.Models
     [Table("DOCTOR")]
     public class Doctor
     {
-        [Column("HOSPITAL_COD")]
-        public int IdHospital { get; set; }
+        //la key siempre la primera propiedad
         [Key]
         [Column("DOCTOR_NO")]
         public int IdDoctor { get; set; }
+        [Column("HOSPITAL_COD")]
+        public int IdHospital { get; set; }
         [Column("APELLIDO")]
         public string Apellido { get; set; }
         [Column("ESPECIALIDAD")]
